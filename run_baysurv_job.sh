d9785ff63fa16f98f454d4b0e2d905dac6077003
Arshia <arshia.tehrani1380@gmail.com>
2026-03-29 17:29:02 -0400

    chore: Extend SLURM job time allocation from 16 hours to 24 hours in run_baysurv_job.sh

--- run_baysurv_job.sh
+++ run_baysurv_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=elec888_train
-#SBATCH --time=0-16:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH --account=def-bakhshai
 #SBATCH --ntasks-per-node=1
 #SBATCH --mail-user=arshia.tehrani1380@gmail.com
